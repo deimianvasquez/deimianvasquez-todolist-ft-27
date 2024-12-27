f1fc60c2d14bb7ccd1971c87d4bb6e7cc66a16b7
Deimian Vásquez <deimianvasquez@gmail.com>
2024-12-27 14:53:01 +0000

    add todos

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,22 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(180), nullable=False)
+    salt = db.Column(db.String(180), nullable=False)
+    avatar = db.Column(db.String(180), default="https://i.pravatar.cc/300")
+    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now(), nullable=False)
+    # todos = db.realationship("Todos", backref="user")
+    todos = db.relationship("Todos", back_populates="user")
 
-    def __repr__(self):
-        return f'<User {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(255), nullable=False)
+    is_done = db.Column(db.Boolean(), default=False)
+    user_id = db.Column(db.Integer(),  db.ForeignKey("user.id"), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now(), nullable=False)
+    user = db.relationship("User", back_populates="todos")
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from base64 import b64encode
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +15,61 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/register", methods=["POST"])
+def add_user():
+    body = request.json
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    name = body.get("name", None)
+    email = body.get("email", None)
+    password = body.get("password", None)
+    avatar = body.get("avatar", None)
 
-    return jsonify(response_body), 200
+    if email is None or password is None or name is None:
+        return jsonify({"message":"Los campos name, email y password son requeridos"}), 400
+    
+    else:
+        user = User()
+        user_exist = User.query.filter_by(email=email).one_or_none()
+        
+        if user_exist is not None:
+            return jsonify({"message": "El usuario ya esta registrado"}), 400
+        
+        salt = b64encode(os.urandom(32)).decode("utf-8")
+        password = generate_password_hash(f"{password}{salt}") # 123456789sṕjṕogjpsdiopojdpofjpdojpsojpfojspodjfpsjodfpj
+
+        user.name = name
+        user.email = email
+        user.password = password
+        user.salt = salt
+        db.session.add(user) # preparandonos para la transacción
+
+        try:
+            db.session.commit()
+            return jsonify({"message":"Usuario registrado exitosamente"}), 201
+        except Exception as err:
+            db.session.rollback()
+            return jsonify({"message":f"Error:{err.args}"}), 500
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    body = request.get_json()
+    email = body.get("email", None)
+    password = body.get("password", None)
+
+    required_fields = {"email", "password"}
+    missing_field = {field for field in required_fields if not body.get(field)}
+
+    if missing_field:
+        return jsonify({"message": f"Estos campos son requeridos {', '.join(missing_field)}"}), 400
+
+    else:
+        user = User.query.filter_by(email=email).one_or_none()
+        if user is None:
+            return jsonify({"message": "Malas credenciales"}), 400
+        else:
+            if check_password_hash(user.password, f"{password}{user.salt}"):
+                token = "hashahahah" # genetrar token
+                return jsonify({"token":token}), 200
+            else:
+                return jsonify({"message": "Malas credenciales"}), 400
